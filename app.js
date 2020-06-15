a7eacdab2c6e70ab2e2286bef9a68e9d53fec9a3
zainab1995786 <65030516+zainab1995786@users.noreply.github.com>
2020-06-15 10:44:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,120 @@
+
+//   Alert
+
+alert("Hello World");
+
+alert("Error!Please enter a valid password.");
+
+alert("Welcome to JS Land...\r\nHappy Coding! ");
+
+alert("Welcome to JS Land...");
+
+var a = 25;
+var b = 15;
+var c = a-b;
+alert(c);
+
+
+// var a = 45;
+// var b = 10;
+// var c = a+b;
+// alert(c);
+
+// console.log("Helo i can run JS through my web browser's consloe");
+
+
+// var a = 30;
+// var b = 17;
+// var c = a+b;
+// console.log(c);
+
+
+// var m = 70
+// var n = ("zainab");
+// var o = n + m;
+// alert(o);
+
+// var v = ("Fatima");
+// var w = ("Zainab");
+// var x = w+v;
+// alert(x);
+
+// var v = ("Fatima");
+// var w = ("Zainab");
+// var x = w+v;
+// console.log(x);
+
+
+// var v = ("Fatima");
+// var w = ("Zainab");
+// var x = w+v;
+// document.write(x);
+
+//     Alert
+
+//    Variable     
+
+//   var a =("User");
+//   var b = ("Name");
+//   var c = a+b;
+//   alert(c);    
+
+// var UserName;
+
+//   var a =("User");
+//   var b = ("Name");
+//   var c = a+b;
+//   document.write(c); 
+
+
+//   var myname=("zainab");
+//   var b = ("fatima");
+//   var c = myname+b;
+//   alert(c) ;
+
+  // var message=("Hello World");
+  // alert(message);
+ 
+
+  // var a=("Jhone Deo");
+  // var b =("15 years old");
+  // var c =("certified Mobile Application Development");
+  // alert(a);
+  // alert(b);
+  // alert(c);
+   
+//   var word ="PIZZA";
+//  alert(" PIZZA \r\n PIZZ \r\n PIZ \r\n PI \r\n P");
+
+
+//  var email=("Example@example.com");
+//  var b =("My email address is");
+//  var c= (b+email)
+//  alert(c);
+
+//  var book =("A smarter way to learn javascript");
+//  var d =("I am trying to learn from the book");
+//  var e = (d + book);
+//  alert(e);
+
+
+// var i = ("Yah! I can writr HTML content through javascript")
+// document.write(i);
+ 
+// var age =("Iam 25 years old");
+// alert(age);
+
+// var p =("you have visited this site 45 time");
+// alert(p);
+
+// var birthYear= ("My birth year is  1995  <br> Datatype of my declared variable number");
+// document.write(birthYear);
+
+// var visitorVisit= ("<strong> Jhone Doe </strong> ordered <strong> 5 T-Shirt</strong>(s) on XYZ clothing store");
+// document.write(visitorVisit);
+
+
+// var variables=("zainab <br> $ <br> _");
+// document.write(variables);
+
+
